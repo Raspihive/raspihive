89ea3776c5c6c946e56bf4a8e6f18c6ad0a4b940
PIOTA89 <paul.schumacher@mailbox.org>
2020-07-17 20:20:32 +0200

    rewrite codebase - added design options + bugfix

--- raspihive3.py
+++ raspihive3.py
@@ -4,18 +4,22 @@ from guizero import App, Window, Combo, Text, TextBox, CheckBox, ButtonGroup, Pu
 import tkinter, time, subprocess, os, sys, getpass, os.path
 import tkinter as tk
 from subprocess import call, Popen, PIPE
-from tkinter import Tk as tk 
+from tkinter import Tk as tk, Menu, FLAT, Label, Entry, Button, W, StringVar, ttk
 import tkinter.simpledialog
 import subprocess as sp, os, getpass, sys  
 import subprocess
 import tkinter as tk
 import tkinter.simpledialog
-###############################################################################
+from guizero import *
 
+###############################################################################
 # Globale Variablen
 localtime = time.asctime( time.localtime(time.time()) )
 ###############################################################################
 # start functions
+
+def hello():
+    print ("hello!")
 
 def update_os_function():
     if os.geteuid() != 0:
@@ -90,57 +94,95 @@ def Ping_function():
 
 def Time_function():
     print("Time: ")
-    text.value = ("Local time is:", localtime)
     
 
-def entertext():
-    displaytext.value = gettext.value
-    print(displaytext.value)
+def show_entry_fields():
+   print("First Name: %s" % (e1.get()))
 
+def showPass():
+    passwordClear.set(password.get())   
 
-# Ask the user if they really want to close the window
-def do_this_when_closed():
-    if app.yesno("Close", "Do you want to quit?"):
-        app.destroy()
 
 ###############################################################################
 # end functions
 
-
 ###############################################################################
 # Start main programm - App-Anfang grid = Spalten und Zeilen
 
-app = App(title="Raspihive", bg = (235, 215, 182), width=320, height=480, layout="grid")
-text = Text(app, text="", size=16, font="Times New Roman", color="black", grid=[2,0], align="top")
-#app.title = ("A different title")
-
-menubar = MenuBar(app,
-                  toplevel=["Update-menu", "Node installer", "Tools"],
-                  options=[
-                      [ ["Update Raspberry Pi", update_os_function], ["Update packages", update_packages_function2] ],
-                      [ ["Install Hornet Node", Hornet_install_function], ["Install GoShimmer", GoShimmer_function] ],
-                      [ ["Ping", Ping_function], ["Show system time", Time_function] ]
-                  ])
+app=App(title='Raspihive',bg=(53, 60, 81), layout="grid")
+root = app.tk
 
 
+#creates menubar
+menubar = Menu(root,relief=FLAT,bd=0)
+
+# Sets menubar background color and active select but does not remove 3d  effect/padding
+menubar.config(bg = "GREEN",fg='white',activebackground='red',activeforeground='pink',relief=FLAT)
+
+# First item on menubar and creates sub options
+filemenu = Menu(menubar, tearoff=0,relief=FLAT, font=("Verdana", 12),activebackground='red')
+filemenu.config(bg = "GREEN") 
+filemenu.add_command(label="Update Raspberry Pi", command=update_os_function)
+filemenu.add_command(label="Update packages", command=update_packages_function2)
+filemenu.add_separator()
+filemenu.add_command(label="Exit", command=root.quit)
+menubar.add_cascade(label="Update-menu", menu=filemenu)
+
+# Adds to menubar and creates sub options
+editmenu = Menu(menubar, tearoff=0, relief=FLAT, font=("Verdana", 12),activebackground='red')
+editmenu.config(bg = "GREEN") 
+editmenu.add_command(label="Install Hornet Node", command=Hornet_install_function)
+editmenu.add_command(label="Install GoShimmer", command=GoShimmer_function)
+menubar.add_cascade(label="Node installer", menu=editmenu)
+
+pingmenu = Menu(menubar, tearoff=0,bg='green',fg='blue')
+pingmenu.add_command(label="Ping", command=Ping_function)
+pingmenu.add_command(label="Show system time", command=Time_function)
+menubar.add_cascade(label="Tools", menu=pingmenu)
+pingmenu.activebackground='red'
 
 
-# insert logo here
-#picture1 = Picture(app, image="/home/paul/Dokumente/Raspihive-Projekt-akt/Python_Codeschnipsel/iota.gif", grid=[0,0])
+helpmenu = Menu(menubar, tearoff=0,bg='green',fg='blue')
+helpmenu.add_command(label="Report bug", command=hello)
+helpmenu.add_command(label="About", command=hello)
+menubar.add_cascade(label="Help", menu=helpmenu)
+helpmenu.activebackground='red'
+
+root.config(menu=menubar)
 
 
-displaytext = Text(app, text="Welcome to Raspihive", size=12, font="Times New Roman", color="black", grid=[0,0])
-gettext = TextBox(app, width=10, grid=[2,0])
-update_text = PushButton(app, command=entertext, text="enter", grid=[6,0])
+"""
+Label(root, text="Raspihive").grid(row=0)
+e1 = Entry(root)
+e1.grid(row=0, column=1)
+Button(root, text='Quit', command=root.quit).grid(row=3, column=0, sticky=W, pady=4)
+Button(root, text='Show', command=show_entry_fields).grid(row=3, column=1, sticky=W, pady=4)
+
+# Label und Buttons erstellen.
+anweisungs_label = Label(root, text="Ich bin eine Anweisung:\n  Klicke auf 'Ändern'.").grid(row=5, column=5, sticky=W, pady=4)
+change_button = Button(root, text="Ändern", command=button_action)
+change_button.grid(row=6, column=5, sticky=W, pady=4)
+change_button.config(anweisungs_label)
+"""
+
+#Test for user display
+password = StringVar()
+passwordClear = StringVar()
+
+label_1 = ttk.Label(root, text='Type your password: ')
+label_1.grid()
+entry_1 = ttk.Entry(root, textvariable=password, width=6, show='*')
+entry_1.grid()
+entry_1.focus()
+label_2 = ttk.Label(root, text='Your password is: ')
+label_2.grid()
+label_3 = ttk.Label(root, textvariable=passwordClear)
+label_3.grid()
+button_1 = ttk.Button(root, text='show', command=showPass, width=6)
+button_1.grid()
 
 
 
 
-
-
-# When the user tries to close the window, run the function do_this_when_closed()
-app.when_closed = do_this_when_closed
 
 app.display()
-###############################################################################
-# End main programm 
