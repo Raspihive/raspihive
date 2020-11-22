bb34ee03f71c0221c396166def906afdd6b22afa
PIOTA89 <paul.schumacher@mailbox.org>
2020-11-22 17:49:57 +0100

    RC1

--- raspihive.py
+++ raspihive.py
@@ -592,7 +592,7 @@ def about():
         messageWindow['bg'] = '#0B3861'
         Label(messageWindow, text="The Plug and Play solution for a Raspberry Pi IOTA Fullnode!", bg="#0B3861", fg="white").grid(row=0, column=0, padx='0', pady='0')
         Label(messageWindow, text="", bg="#0B3861", fg="white").grid(row=1, column=0, padx='0', pady='0')
-        Label(messageWindow, text="Raspihive: Beta Version 4.0", bg="#0B3861", fg="white").grid(row=2, column=0, padx='0', pady='0')
+        Label(messageWindow, text="Raspihive: RC 1.0", bg="#0B3861", fg="white").grid(row=2, column=0, padx='0', pady='0')
         Label(messageWindow, text="", bg="#0B3861", fg="white").grid(row=3, column=0, padx='0', pady='0')
         
         Label(messageWindow, text="", bg="#0B3861", fg="white").grid(row=4, column=0, padx='0', pady='0')
