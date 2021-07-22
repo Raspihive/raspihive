e92e4abb8557f6d37d0b34ab694ac21c34688079
NO8ODY <steffen@emayl.me>
2021-07-22 20:26:43 +0200

    fix missing ufw rules

--- raspihive/threads.py
+++ raspihive/threads.py
@@ -88,6 +88,7 @@ class MyThread_hornet_update(QThread):
             && sudo apt-get update && sudo apt-get -y upgrade hornet \
             && sudo wget -q -O /usr/bin/hornet https://tanglebay.com/assets/hornet-arm64 \
             && sudo wget -q -O /var/lib/hornet/config.json https://tanglebay.com/assets/config.json \
+            && sudo ufw allow 14626/udp \
             sudo systemctl restart hornet"), stdout=subprocess.PIPE, shell = True)
 
         p = process.stdout.readline()
@@ -127,6 +128,7 @@ class MyThread_hornet_install(QThread):
             && sudo apt-get install hornet && sudo systemctl enable hornet.service \
             && sudo wget -q -O /usr/bin/hornet https://tanglebay.com/assets/hornet-arm64 \
             && sudo wget -q -O /var/lib/hornet/config.json https://tanglebay.com/assets/config.json \
+            && sudo ufw allow 14626/udp \
             && sudo service hornet start '), stdout=subprocess.PIPE, shell = True)
         
 
