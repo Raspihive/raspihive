c4481444ef5b7bc21ce50b0ab90bf705c89e5c4f
PIOTA89 <50791571+PIOTA89@users.noreply.github.com>
2021-10-30 17:59:53 +0200

    fix bug

--- raspihive/progress_bars.py
+++ raspihive/progress_bars.py
@@ -1,16 +1,24 @@
-
 ###############################################################################
 # libraries
 from PyQt5.QtWidgets import QProgressBar, QDialog
 from PyQt5.QtWidgets import (QVBoxLayout)
-
-from .threads import *
+from os import path
+from .threads import (
+    MyThread_os_update,
+    MyThread_packages,
+    MyThread_hornet_update,
+    MyThread_hornet_install,
+    MyThread_hornet_uninstall,
+    MyThread_nginx_certbot_install,
+    MyThread_nginx_certbot_uninstall
+)
 
 ##############################################################################
 #Progress bar for OS Update
 class Window_os_update(QDialog):
     def __init__(self):
         super().__init__()
+
         self.setStyleSheet('background-color: #2B3440; color: black;')
         self.title = "OS Update"
         self.top = 200
@@ -36,7 +44,6 @@ class Window_os_update(QDialog):
         self.setLayout(vbox)
         self.show()
 
-    #def startProgressBar():
         self.thread = MyThread_os_update()
         self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
@@ -89,51 +96,16 @@ class Window_packages(QDialog):
 ###############################################################################
 
 ###############################################################################
-#Progress bar for hornet update
-class Window_hornet_update(QDialog):
+#Progress bar for hornet
+class HornetProgress(QDialog):
+    """
+    All Hornet UI activities to be triggered from here.
+    """
+
     def __init__(self):
         super().__init__()
-        self.setStyleSheet('background-color: #2B3440; color: black;') #rgb(255,255,255);
-        self.title = "Hornet Update"
-        self.top = 200
-        self.left = 500
-        self.width = 300
-        self.height = 100
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        vbox = QVBoxLayout()
-        self.progressbar = QProgressBar()
-        #self.progressbar.setOrientation(Qt.Vertical)
-        self.progressbar.setMaximum(100)
-        #self.progressbar.setStyleSheet("QProgressBar \
-        # {border: 2px solid grey;border-radius:8px;\
-        # padding:1px}""QProgressBar::chunk {background:black}")
-        #qlineargradient(x1: 0, y1: 0.5, x2: 1, y2: \
-        # 0.5, stop: 0 red, stop: 1 white);
-        self.progressbar.setStyleSheet("QProgressBar::chunk \
-            {background: qlineargradient(x1: 0, \
-                y1: 0.5, x2: 1, y2: 0.5, stop: 0 lightblue, stop: 1 lightblue); }")
-        #self.progressbar.setTextVisible(False)
-        vbox.addWidget(self.progressbar)
-        #self.startProgressBar(self)
-        self.setLayout(vbox)
-        self.show()
 
-    #def startProgressBar():
-        self.thread = MyThread_hornet_update()
-        self.thread.change_value.connect(self.setProgressVal)
-        self.thread.start()
-
-    def setProgressVal(self, val):
-        self.progressbar.setValue(val)
-#End of Progress bar for hornet update
-###############################################################################
-
-###############################################################################
-#Progress bar for hornet install
-class Window_hornet_install(QDialog):
-    def __init__(self):
-        super().__init__()
+    def install(self):
         self.setStyleSheet('background-color: #2B3440; \
         color: black;') #rgb(255,255,255);
         self.title = "Hornet install"
@@ -161,21 +133,49 @@ class Window_hornet_install(QDialog):
         self.setLayout(vbox)
         self.show()
 
-    #def startProgressBar():
-        self.thread = MyThread_hornet_install()
+    # def startProgressBar():
+        thread_cmd = MyThread_hornet_install()
+        if thread_cmd:
+            self.thread = thread_cmd
+            self.thread.change_value.connect(self.setProgressVal)
+            self.thread.start()
+        else:
+            return None
+
+    def update(self):
+        self.setStyleSheet('background-color: #2B3440; color: black;') #rgb(255,255,255);
+        self.title = "Hornet Update"
+        self.top = 200
+        self.left = 500
+        self.width = 300
+        self.height = 100
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        vbox = QVBoxLayout()
+        self.progressbar = QProgressBar()
+        #self.progressbar.setOrientation(Qt.Vertical)
+        self.progressbar.setMaximum(100)
+        #self.progressbar.setStyleSheet("QProgressBar \
+        # {border: 2px solid grey;border-radius:8px;\
+        # padding:1px}""QProgressBar::chunk {background:black}")
+        #qlineargradient(x1: 0, y1: 0.5, x2: 1, y2: \
+        # 0.5, stop: 0 red, stop: 1 white);
+        self.progressbar.setStyleSheet("QProgressBar::chunk \
+            {background: qlineargradient(x1: 0, \
+            y1: 0.5, x2: 1, y2: 0.5, stop: 0 lightblue, \
+            stop: 1 lightblue); }")
+        #self.progressbar.setTextVisible(False)
+        vbox.addWidget(self.progressbar)
+        #self.startProgressBar(self)
+        self.setLayout(vbox)
+        self.show()
+
+        # def startProgressBar():
+        self.thread = MyThread_hornet_update()
         self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
 
-    def setProgressVal(self, val):
-        self.progressbar.setValue(val)
-#End of Progress bar for hornet install
-###############################################################################
-
-###############################################################################
-#Progress bar for hornet uninstall
-class Window_hornet_uninstall(QDialog):
-    def __init__(self):
-        super().__init__()
+    def uninstall(self):
         self.setStyleSheet('background-color: #2B3440; \
         color: black;') #rgb(255,255,255);
         self.title = "Hornet uninstall"
@@ -203,21 +203,24 @@ class Window_hornet_uninstall(QDialog):
         self.setLayout(vbox)
         self.show()
 
-    #def startProgressBar():
+        # def startProgressBar():
         self.thread = MyThread_hornet_uninstall()
         self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
 
     def setProgressVal(self, val):
         self.progressbar.setValue(val)
-#End of Progress bar for hornet uninstall
+
+# End of Progress bar for hornet
 ###############################################################################
 
 ###############################################################################
-#Progress bar for nginx+certbot install
-class Window_nginx_certbot_install(QDialog):
+# Progress bar for nginx+certbot
+class NGINXCertbot(QDialog):
     def __init__(self):
         super().__init__()
+
+    def install(self):
         self.setStyleSheet('background-color: #2B3440; \
         color: black;') #rgb(255,255,255);
         self.title = "Nginx + Certbot install"
@@ -249,16 +252,7 @@ class Window_nginx_certbot_install(QDialog):
         self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
 
-    def setProgressVal(self, val):
-        self.progressbar.setValue(val)
-#End of Progress bar for nginx+certbot install
-###############################################################################
-
-###############################################################################
-#Progress bar for nginx+certbot uninstall
-class Window_nginx_certbot_uninstall(QDialog):
-    def __init__(self):
-        super().__init__()
+    def uninstall(self):
         self.setStyleSheet('background-color: #2B3440; \
         color: black;') #rgb(255,255,255);
         self.title = "Nginx + Certbot uninstall"
@@ -286,11 +280,12 @@ class Window_nginx_certbot_uninstall(QDialog):
         self.setLayout(vbox)
         self.show()
 
-    #def startProgressBar():
+        #def startProgressBar():
         self.thread = MyThread_nginx_certbot_uninstall()
         self.thread.change_value.connect(self.setProgressVal)
         self.thread.start()
 
     def setProgressVal(self, val):
         self.progressbar.setValue(val)
-#End of Progress bar for nginx+certbot uninstall
+# End of Progress bar for nginx+certbot
+###############################################################################
